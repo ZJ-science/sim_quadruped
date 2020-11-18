734e9be47087a4ca57da3eeaa4dde1bbc08d9ac8
ZJ-science <985324779@qq.com>
2020-11-18 11:32:12 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zj-robot/catkin_ws/src
+# Build directory: /home/zj-robot/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mini_2")
---
+++ build/mini_2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zj-robot/catkin_ws/src/mini_2
+# Build directory: /home/zj-robot/catkin_ws/build/mini_2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
